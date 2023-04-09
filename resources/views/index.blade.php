be8984379264919c7beeae8ac1d1c81728bd90a0
Argenis Sanchez <arsanchez@addi.com>
2023-04-08 21:25:48 -0400

    Fixed the home layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
 <h4 class="text-danger">Error {{$errors->first()}}</h4>
 @endif
 <div class="row">
-    <div class="col-12">
-      <a href="/subscribers/create" class="btn btn-success">Add new subscriber</a>
-    </div>
     @if ($settings == true)
+        <div class="col-12">
+          <a href="/subscribers/create" class="btn btn-success">Add new subscriber</a>
+        </div>
         <x-table>
         </x-table>
     @else 
